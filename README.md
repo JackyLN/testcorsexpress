1f12fe278567e56c2a9e5ca7033ca7eee0f41460
JackyLN <lenghia1991@gmail.com>
2020-07-31 06:33:56 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+### Simple setup to test out CORS middleware with Express Router
+
+-testcors.html to run on local
+
+-hosting by running 
+$> yarn
+$> node server
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = 2345;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send("This is a test API for checking CORS middleware with Express router")
+})
+
+const AdminRouter = require('./src/services/AdminServices');
+app.use('/admin', AdminRouter);
+
+//App Start
+app.listen(port, () => console.log(`API running on PORT ${port}!`))
